cead00115b4d1683dbc37578eb495d0dba4cf3e5
Diego C <delcaos12@gmail.com>
2024-03-22 10:48:11 -0500

    First commit

---
+++ EjemploDefontana/Program.cs
@@ -0,0 +1,120 @@
+﻿//Respuestas
+//1-1.
+var list = new List<int>();
+var random = new Random();
+
+for (int i = 0; i < 10000000; i++)
+{
+    int value = random.Next(-100000, 100001);
+    list.Add(value);
+}
+
+// foreach (var num in list)
+// {
+//     Console.WriteLine(num);
+// }
+
+//1-2.
+var cantidadElementos = list.Count;
+Console.WriteLine($"{cantidadElementos}");
+
+var valorMax = list.Max();
+Console.WriteLine($"{valorMax}");
+
+var valorMin = list.Min();
+Console.WriteLine($"{valorMin}");
+
+var average = list.Average();
+Console.WriteLine($"{average}");
+
+
+//1-3.
+var valoresCero = list.Where(n => n == 0).Count();
+Console.WriteLine($"{valoresCero}");
+
+//1-4. 
+var rankingValores =
+    list.GroupBy(n => n)
+        .ToDictionary(n => n.Key, n => n.Count())
+        .OrderByDescending(n => n.Value)
+        .Take(5);
+
+Console.WriteLine($"{string.Join(", ", rankingValores)}");
+
+
+
+//1-5.
+var array = new List<int>();
+for (int i = 0; i < list.Count; i++)
+{
+    List<int> arrMayores = array.Where(n => n >= list[i]).ToList();
+    if (arrMayores.Count == 0 || i == 0)
+    {
+        array.Add(list[i]);
+    }
+    else
+    {
+        int valor = arrMayores.First();
+        int index = array.IndexOf(valor);
+        array.Insert(index, list[i]);
+    }
+}
+
+foreach (var val in array)
+    Console.WriteLine(val);
+
+// //1.6
+var valoresParesModificados = list.Select(n => n % 2 != 0 ? n + 1 : n);
+Console.WriteLine($"{valoresParesModificados}");
+
+
+// //2-1.
+var characters = "abcdefghijklmnopqrstuvwxyz";
+var charsarr = new char[4];
+List<string> words = new List<string>();
+
+for (double j = 0; j < 10000000; j++)
+{
+    for (int i = 0; i < charsarr.Length; i++)
+    {
+        charsarr[i] = characters[random.Next(characters.Length)];
+    }
+
+    var resultString = new String(charsarr);
+    words.Add(resultString);
+}
+
+foreach (var w in words)
+{
+    Console.WriteLine(w);
+}
+
+// //2-2
+int numHolas = words.Count(n => n.Equals("hola"));
+
+Console.WriteLine(numHolas);
+
+// //3-1
+int simulaciones = 1000000;
+Random rnd = new Random();
+int contador = 0;
+const int totalPersonas = 23;
+
+for (int i = 0; i < simulaciones; i++)
+{
+    var fechas = new List<int>();
+
+    for (int j = 0; j < totalPersonas; j++)
+    {
+        int fechaRandom = rnd.Next(1, 366);
+        fechas.Add(fechaRandom);
+    }
+
+    var fechasSinDuplicado = fechas.Distinct().ToList();
+
+    if (fechasSinDuplicado.Count < totalPersonas) contador++;
+}
+
+double probabilidadPromedio = (double)contador / simulaciones;
+
+Console.WriteLine($"Probabildad para 1 millón de escenarios: {probabilidadPromedio:P}");
---
+++ Readme.md
@@ -0,0 +1,32 @@
+
+## Actividad resuelta
+
+### Genera una aplicación de consola que realice lo siguiente:
+
+- 1-1.- Genera una lista o arreglo de 10.000.000 números enteros aleatorios entre -100.000 y 100.000
+
+- 1-2.- Imprime en distintas líneas por consola los siguientes datos:
+    - La cantidad total de elementos del arreglo.
+   - El valor más alto en el arreglo.
+   - El valor más bajo en el arreglo.
+   - El promedio de todos los valores en el arreglo.
+    
+- 1-3.- Imprime la cantidad de veces que se obtuvo el valor 0.
+
+- 1-4.- Imprime los 5 valores que más se repiten desde el más repetido al que menos lo hace, indicando para cada uno cuántas veces se repite.
+
+- 1-5.- Ordena el arreglo desde el valor más pequeño al más alto, con cualquier algoritmo de ordenamiento, implementado por ti (sin usar Sort(), OrderBy(), o métodos similares).
+    
+- 1-6.- Modifica todos los valores impares del arreglo, y reemplazalos por cualquier valor par.
+   
+- 2-1.- Genera una lista de 10.000.000 palabras, de 4 letras minúsculas cada una (de la a a la z, sin ñ)
+    - (caracteres del 97 al 122)
+           char aleatorio = 97 + random(26);
+
+- 2-2.- Imprime la cantidad de veces que se generó la palabra 'hola'.
+
+- 3-1.- Paradoja del cumpleaños: https://es.wikipedia.org/wiki/Paradoja_del_cumplea%C3%B1os
+    
+    - "Demuestra" la pradoja del cumpleaños con un programa que genere 23 fechas de cumpleaños al azar, 1 millón de veces.
+   - Paradoja del cumpleaños:
+        El problema del cumpleaños, también llamado paradoja del cumpleaños, establece que de un conjunto de 23 personas, hay una probabilidad del 50,7% de que al menos dos de ellas cumplan años el mismo día.
